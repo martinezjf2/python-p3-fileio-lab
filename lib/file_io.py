524f4998a11db477a722bf877e3321733817cb0f
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-02 17:28:26 -0400

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", 'w') as f:
+        f.write(file_content)
+    f.close()
     pass
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", 'a') as f:
+        f.write(append_content)
+    f.close()
     pass
 
 def read_file(file_name):
+    with open(f"{file_name}.txt") as f:
+        for line in f:
+            return line
     pass
